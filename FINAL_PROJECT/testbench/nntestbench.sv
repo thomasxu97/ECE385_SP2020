789a1ee67deb36e12692e63f15b14fb38683a781
Luotian Xu <luotian2@linux-40.ews.illinois.edu>
2020-05-05 16:33:38 -0500

    fix multiplier

--- FINAL_PROJECT/testbench/nntestbench.sv
+++ FINAL_PROJECT/testbench/nntestbench.sv
@@ -3,12 +3,14 @@ module nntestbench();
 timeunit 10ns;	// Half clock cycle at 50 MHz
 			// This is the amount of time represented by #1 
 timeprecision 1ns;
-logic [31:0] a, b, f;
+logic [31:0] a, b, f, r;
 multiplier mult(.a(a), .b(b), .f(f));
+
 
 initial begin
 #1 a = 32'h4148A3D7;
 #1 b = 32'h42B2428F;
+#1 r = 32'h448BB616;
 end
 
 endmodule 
