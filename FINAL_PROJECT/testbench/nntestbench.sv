bce4853d84ba474a50cce1308158167ec3733e7b
Luotian Xu <luotianxu@xuluotians-MacBook-Pro.local>
2020-05-07 01:34:26 -0500

    unit test argmax

--- FINAL_PROJECT/testbench/nntestbench.sv
+++ FINAL_PROJECT/testbench/nntestbench.sv
@@ -3,12 +3,24 @@ module nntestbench();
 timeunit 10ns;	// Half clock cycle at 50 MHz
 			// This is the amount of time represented by #1 
 timeprecision 1ns;
-logic [31:0] a, b, f;
-multiplier u_mult(.a(a), .b(b), .f(f));
+logic [31:0] a [9:0];
+logic [4:0] f;
+argmax argmax (
+	.in (a),
+	.number (f)
+	);
 
 initial begin
-#1 a = 32'hC23D70A3;
-#1 b = 32'h00000000;
+#1 a[0] = 32'h00000000;
+#1 a[1] = 32'h00000000;
+#1 a[2] = 32'h00000000;
+#1 a[3] = 32'h00000000;
+#1 a[4] = 32'h00000000;
+#1 a[5] = 32'h00000000;
+#1 a[6] = 32'h00000000;
+#1 a[7] = 32'h00000000;
+#1 a[8] = 32'h00000000;
+#1 a[9] = 32'h4841B347;
 end
 
 endmodule 
